d6fba5069c7600b5372c531cba5372a9ba997f90
Charles Peden <ccbpeden@warpmail.net>
2018-03-18 10:07:48 -0700

    change header text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function liveTime(){
 	
-	var greeting = React.createElement('h1', {}, 'Hello, World!');
+	var greeting = React.createElement('h1', {}, 'Help Queue');
 	var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
 	var app = React.createElement('div', {}, greeting, clock);
 	
