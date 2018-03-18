451b5ed43859aa535ee8128ca4c9038f6d26bec5
Charles Peden <ccbpeden@warpmail.net>
2018-03-18 10:15:15 -0700

    add new elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 function liveTime(){
 	
-	var greeting = React.createElement('h1', {}, 'Help Queue');
+	var heading = React.createElement('h1', {}, 'Help Queue');
+	var ticketLocation = React.createElement('h3', {}, '3a');
+	var ticketName = React.createElement('h3', {}, 'Thato & Haley');
+	var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
 	var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-	var app = React.createElement('div', {}, greeting, clock);
+	var app = React.createElement('div', {}, heading, ticketLocation, ticketName, ticketIssue);
 	
 	ReactDOM.render(
 		app,
